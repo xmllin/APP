6e0a099c85786db419f1faaf45cc6daa02a5f867
xmllin <xmlin717@gmail.com>
2026-09-22 09:58:46 +0300

    feat: allow responsive pagination page sizes

--- Services/PaginationState.cs
+++ Services/PaginationState.cs
@@ -14,13 +14,22 @@ namespace WpfApp1.Services
             PageSize = pageSize;
         }
 
-        public int PageSize { get; }
+        public int PageSize { get; private set; }
         public int CurrentPage { get; private set; } = 1;
         public int PageCount => Math.Max(1, (int)Math.Ceiling(_items.Count / (double)PageSize));
         public bool HasPreviousPage => CurrentPage > 1;
         public bool HasNextPage => CurrentPage < PageCount;
         public bool HasMultiplePages => _items.Count > PageSize;
         public string PageLabel => CurrentPage + " / " + PageCount;
+
+        public void SetPageSize(int pageSize, bool resetToFirstPage = false)
+        {
+            if (pageSize < 1) throw new ArgumentOutOfRangeException(nameof(pageSize));
+            if (PageSize == pageSize && !resetToFirstPage) return;
+
+            PageSize = pageSize;
+            CurrentPage = resetToFirstPage ? 1 : Math.Min(CurrentPage, PageCount);
+        }
 
         public void SetItems(IEnumerable<T> items, bool resetToFirstPage = false)
         {
